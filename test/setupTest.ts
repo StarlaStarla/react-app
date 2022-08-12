a793e28a54736901b9625e7b3910daf1e0279b62
StarlaStarla <35161544+StarlaStarla@users.noreply.github.com>
2022-08-12 08:40:44 +0000

    Deploying to gh-pages from @ StarlaStarla/react-app@3ab594794b1e98717238b5b1a70f9e8bca869bda 🚀

--- test/setupTest.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom'
-import { jestPreviewConfigure } from 'jest-preview'
-
-// Should be path from root of your project
-jestPreviewConfigure({
-  publicFolder: './src/assets', // No need to configure if `publicFolder` is `public`
-  autoPreview: true,
-})
