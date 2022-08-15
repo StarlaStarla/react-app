80407c46d6d1c53a8ae75be18021f2c97a0629df
Starla Huang <starla_huang@epam.com>
2022-08-15 14:27:28 +0800

    fix: lint-staged test files config

--- jest.config.ts
+++ jest.config.ts
@@ -155,7 +155,11 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+  testMatch: [
+    '<rootDir>\\src\\**\\*.{spec,test}.{js,jsx,ts,tsx}',
+    '<rootDir>\\src\\**\\__tests__\\**\\*.{spec,test}.{js,jsx,ts,tsx}',
+    '<rootDir>\\__test__\\**\\*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
